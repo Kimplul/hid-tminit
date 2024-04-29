3c604578073aa04c86119bbf17eb4e2b515ed592
Yassine Imounachen <yassine.imounachen@gmail.com>
2024-04-29 03:01:57 +0100

    add support for TS-XW initialization

--- usb-tminit.c
+++ usb-tminit.c
@@ -31,6 +31,7 @@ static void thrustmaster_usb_disconnect(struct usb_interface *interface)
 
 static const struct usb_device_id thrustmaster_usb_devices[] = {
 	{ USB_DEVICE(0x044f, 0xb69c) },
+	{ USB_DEVICE(0x044f, 0xb691) },
 	{}
 };
 
